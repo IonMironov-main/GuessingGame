665fcdfaa909d70f5ba3e15346e6e970bfcb7190
Ivon Mironov <mironov.ion@gmail.com>
2023-05-24 14:53:19 -0400

    Update 2

--- Program.cs
+++ Program.cs
@@ -7,7 +7,18 @@
             int answer = 7;
             Console.WriteLine("Guess a number between 1 and 10:");
 
+            int userGuess = Convert.ToInt32(Console.ReadLine());
 
+            if (userGuess == answer)
+            {
+                Console.WriteLine("How'd you know?!");
+            }
+            else if (userGuess > 10)
+            {
+                Console.WriteLine("Your answer must be between 1 and 10");
+            }
+            else
+                Console.WriteLine("Sorry, that's incorrect.");
         }
     }
 }
