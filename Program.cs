345ec9ae46eea09222f010ea3ace0bbd94b795e0
Ivon Mironov <mironov.ion@gmail.com>
2023-05-26 11:14:49 -0400

    Rackean's update

--- Program.cs
+++ Program.cs
@@ -5,29 +5,36 @@
         static void Main(string[] args)
         {
             int answer = 7;
-
             int userGuess;
             int count = 4;
 
             do
             {
-                Console.WriteLine("Guess a number between 1 and 10:");
+                Console.WriteLine("Guess a number between 1 and 10");
                 userGuess = Convert.ToInt32(Console.ReadLine());
 
                 if (userGuess == answer)
                 {
                     Console.WriteLine("How'd you know?!");
                 }
-
                 else if (userGuess > 10)
                 {
-                    Console.WriteLine("Your answer must be between 1 and 10.");
-                }
+                    Console.WriteLine("Your answer must be between 1 and 10");
 
-                else
+                }
+                else if (answer != userGuess)
                 {
-                    Console.WriteLine($"Sorry, that's incorrect.You have {count - 1} attempt(s) left.");
-                    count--;
+                    if (answer > userGuess)
+                    {
+                        Console.WriteLine($"Sorry, that's incorrect. You were {answer - userGuess} digits away from the correct answer. You now have {count - 1} attempt(s) left");
+                        count--;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Sorry, that's incorrect. You were {userGuess - answer} digits away from the correct answer. You now have {count - 1} attempt(s) left");
+                        count--;
+                    }
+
                     if (count == 0)
                     {
                         Console.WriteLine("You have reached your max amount of tries. Sorry!");
